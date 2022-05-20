f26ece1ef644f9d26b3ee309fc74c6147abde4e7
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-05-20 13:20:52 -0300

    fix: pequenas alterações no código.

--- movies_crud/schema.py
+++ movies_crud/schema.py
@@ -11,15 +11,9 @@ class MovieBase(BaseModel):
 
 class MovieAdd(MovieBase):
     movie_id: str
-    # Optional[str] is just a shorthand or alias for Union[str, None].
-    # It exists mostly as a convenience to help
-    # function signatures look a little cleaner.
     streaming_platform: Optional[str] = None
     membership_required: bool
 
-    # Behaviour of pydantic can be controlled via the Config class on a model
-    # to support models that map to ORM objects.
-    # Config property orm_mode must be set to True
     class Config:
         orm_mode = True
 
@@ -27,22 +21,13 @@ class MovieAdd(MovieBase):
 class Movie(MovieAdd):
     id: int
 
-    # Behaviour of pydantic can be controlled via the Config class on a model
-    # to support models that map to ORM objects.
-    # Config property orm_mode must be set to True
     class Config:
         orm_mode = True
 
 
 class UpdateMovie(BaseModel):
-    # Optional[str] is just a shorthand or alias for Union[str, None].
-    # It exists mostly as a convenience to help function
-    # signatures look a little cleaner.
     streaming_platform: Optional[str] = None
     membership_required: bool
 
-    # Behaviour of pydantic can be controlled via the Config class on a model
-    # to support models that map to ORM objects.
-    # Config property orm_mode must be set to True
     class Config:
         orm_mode = True
